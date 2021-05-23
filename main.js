07f9c476a42239312e8ea576dc15859f760b5701
WillTechX20 <75710436+WillTechX20@users.noreply.github.com>
2021-05-23 11:06:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var canvas=null;
+var video=null;
+var newImageClassifier=null;
+
+function preload(){
+}
+
+function onModelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function gotResult(){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.querySelector('#result_object_name').innerHTML=results[0].label;
+        document.querySelector('#result_object_name').innerHTML=results[0].label.confidence.toFixed(3);
+    }
+}
+
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    newImageClassifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NmIQ1uMTv/model.json', onModelLoaded);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    newImageClassifier.classifiy(video, gotResult);
+}
